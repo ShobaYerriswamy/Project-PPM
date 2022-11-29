bd3d41a05a75d69a1d227483d5582259977d9b63
ShobaYerriswamy <119297948+ShobaYerriswamy@users.noreply.github.com>
2022-11-29 18:11:05 +0530

    Update PrilificsProjectManager.cs.cs

--- PrilificsProjectManager.cs.cs
+++ PrilificsProjectManager.cs.cs
@@ -1,5 +1,79 @@
 using System;
 
+public class PrilificsProjectManager
+{
+
+    public static void Main(string[]args)
+    {
+        Console.WriteLine("Hello from the console PPM");
+        Console.WriteLine("Enter 1 to Add Project");
+        Console.WriteLine("Enter 2 to View Projects");
+        Console.WriteLine("Enter 3 to Add Employee");
+        Console.WriteLine("Enter 4 to View Employees");
+        Console.WriteLine("Enter 5 to Add Role");
+        Console.WriteLine("Enter 6 to View Roles");
+        Console.WriteLine("Enter 'S' to QUIT the application");
+
+        var userInput = Console.ReadLine();
+            
+        ProjectManager PM =  new ProjectManager();
+        EmployeeManager EM = new EmployeeManager();
+        RoleManager RM = new RoleManager();
+
+        while (true)
+        {
+            switch (userInput)
+            {
+                case "1":
+                    Console.WriteLine("Enter the Project Name");
+                    var add = Console.ReadLine();
+                    var newProject = new Project(add);
+                    PM.AddProject(newProject);
+                    Console.WriteLine("Project Added Successfully");
+                    break;
+                
+                case "2":
+                    PM.ViewProject();
+                    break;
+
+                case "3":
+                    Console.WriteLine("Enter the Employee Name");
+                    var add1 = Console.ReadLine();
+                    var newEmployee = new Employee(add1);
+                    EM.AddEmployee(newEmployee);
+                    Console.WriteLine("Employee Added Successfully");
+                    break;
+                
+                case "4":
+                    EM.ViewEmployee();
+                    break;
+
+                case "5":
+                    Console.WriteLine("Enter the Role Name");
+                    var add2 = Console.ReadLine();
+                    var newRole = new Role(add2);
+                    RM.AddRole(newRole);
+                    Console.WriteLine("Role Added Successfully");
+                    break;
+                  
+                case "6":
+                    RM.ViewRole();
+                    break;
+                    
+                case "S":
+                    return;
+
+                default:
+                    Console.WriteLine("Select Valid Operation");
+                    break;
+            }   
+
+            Console.WriteLine("Select Operation");
+            userInput = Console.ReadLine();
+        }
+    }
+}
+
 public class Project
 {
     public string ProjectName { get; set; }
@@ -7,7 +81,7 @@ public class Project
     public Project(string projectname)
     {
         ProjectName = projectname;
-    }
+    } 
 }
 
 public class ProjectManager
@@ -19,7 +93,7 @@ public class ProjectManager
         Projects.Add(project);
     }
     
-    public void displayAllProjects(Project project)
+     public void displayAllProjects(Project project)
     {
         Console.WriteLine("[" + project.ProjectName + "]");
     }
@@ -32,7 +106,6 @@ public class ProjectManager
         }
     }
 }
-
 public class Employee
 {
     public string EmployeeName { get; set; }
@@ -51,7 +124,6 @@ public class EmployeeManager
     {
         Employees.Add(employee);
     }
-    
      public void displayAllEmployees(Employee employee)
     {
         Console.WriteLine("[" + employee.EmployeeName + "]");
@@ -95,80 +167,6 @@ public class RoleManager
         foreach (var i in Roles)
         {
             displayAllRoles(i);
-        }
-    }
-}
-
-public class PrilificsProjectManager
-{
-
-    public static void Main(string[]args)
-    {
-        Console.WriteLine("Hello from the console PPM");
-        Console.WriteLine("Enter 1 to Add Project");
-        Console.WriteLine("Enter 2 to View Projects");
-        Console.WriteLine("Enter 3 to Add Employee");
-        Console.WriteLine("Enter 4 to View Employees");
-        Console.WriteLine("Enter 5 to Add Role");
-        Console.WriteLine("Enter 6 to View Roles");
-        Console.WriteLine("Enter 'S' to QUIT the application");
-
-        var userInput = Console.ReadLine();
-            
-        ProjectManager PM =  new ProjectManager();
-        EmployeeManager EM = new EmployeeManager();
-        RoleManager RM = new RoleManager();
-
-        while (true)
-        {
-            switch (userInput)
-            {
-                case "1":
-                    Console.WriteLine("Enter the Project Name");
-                    var add = Console.ReadLine();
-                    var newProject = new Project(add);
-                    PM.AddProject(newProject);
-                    Console.WriteLine("Project Added Successfully!");
-                    break;
-                
-                case "2":
-                    PM.ViewProject();
-                    break;
-
-                case "3":
-                    Console.WriteLine("Enter the Employee Name");
-                    var add1 = Console.ReadLine();
-                    var newEmployee = new Employee(add1);
-                    EM.AddEmployee(newEmployee);
-                    Console.WriteLine("Employee Added Successfully!");
-                    break;
-                
-                case "4":
-                    EM.ViewEmployee();
-                    break;
-
-                case "5":
-                    Console.WriteLine("Enter the Role Name");
-                    var add2 = Console.ReadLine();
-                    var newRole = new Role(add2);
-                    RM.AddRole(newRole);
-                    Console.WriteLine("Role Added Successfully!");
-                    break;
-                  
-                case "6":
-                    RM.ViewRole();
-                    break;
-                    
-                case "S":
-                    return;
-
-                default:
-                    Console.WriteLine("Select Valid Operation");
-                    break;
-            }   
-
-            Console.WriteLine("Select Operation");
-            userInput = Console.ReadLine();
         }
     }
 }
